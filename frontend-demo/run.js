0238d974439569afcc7b4ec2ebe798bea8a824a9
michaeldu1 <mdu7@stanford.edu>
2020-10-12 01:33:15 -0700

    Michael's week 5 demo

---
+++ frontend-demo/run.js
@@ -0,0 +1,84 @@
+const puppeteer = require('puppeteer');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const prompt = require('prompt-sync')();
+
+console.log("Hey there, I'm Alex, and I'm here to help you with Amazon Customer Support!\n\
+  Type QUIT to quit, otherwise feel free to ask any questions!");
+
+function askQuestion() {
+  readline.question('(Alex): How can I help you today?\n ', async function(question){
+    if (question == 'QUIT') {
+      console.log("Bye!")
+      readline.close();
+    } else {
+      await search(question);
+      askQuestion();
+    }  
+  });
+}
+
+async function expandSearch(searchUri) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(searchUri);
+  let searchResults = await page.evaluate(() => {
+    let elements = document.getElementsByClassName('help-content');
+    let data = [];
+
+    for (var element of elements) {
+      data.push([element.textContent.trim(), element.href])
+    }
+    return data;
+  });
+  console.log(searchResults[0].toString().replace(/\n\s*\n/g, '\n'));
+  browser.close();
+}
+
+function selectResponse(searchResults, numResults) {
+  let choice = -1;
+  if (numResults == 0){
+    console.log("Sorry, we couldn't find what you were looking for!");
+  } else if(numResults == 1){
+    choice = 0;
+  } else if(numResults == 2){
+    console.log(`The top search results are: \n1. ${searchResults[0][0]}, \n2. ${searchResults[1][0]}`);
+    choice = prompt("Which would you like more information on (1 or 2)? ")
+  } else{
+    console.log(`The top 3 search results are: \n1. ${searchResults[0][0]}, \n2. ${searchResults[1][0]}, \n3. ${searchResults[2][0]}`);
+    choice = prompt("Which would you like more information on (1, 2, 3)? ")
+  }
+  return choice - 1
+}
+
+async function search(query) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    let formattedQuery = query.replace(/\s+/g, '+');
+    let searchQuery = `https://www.amazon.com/gp/help/customer/display.html/ref=hp_search_rd_gw?__mk_en_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&help_keywords=${formattedQuery}&search=true&nodeId=508510&kwHidden=true&sprefix=&locale=en_US`;
+    await page.goto(searchQuery);
+
+    let searchResults = await page.evaluate(() => {
+      let elements = document.getElementsByClassName('a-link-normal');
+      let data = [];
+  
+      for (var element of elements) {
+        data.push([element.textContent.trim(), element.href])
+      }
+      return data;
+    });
+    let choice = -1;
+    let searchAgain = "y";
+    while (searchAgain == "y"){
+      choice = selectResponse(searchResults, searchResults.length);
+      await expandSearch(searchResults[choice][1]);
+      searchAgain = prompt("\nWould you like to revisit the search results (y/n)? ")
+    }
+    browser.close();
+}
+
+askQuestion();
+
